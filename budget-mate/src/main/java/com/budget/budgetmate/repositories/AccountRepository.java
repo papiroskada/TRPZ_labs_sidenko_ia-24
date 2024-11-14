deb55e23ce682a26015783d6ea928219b0f715d1
dashushak <100213341+papiroskada@users.noreply.github.com>
2024-11-14 11:14:41 +0300

    Delete budget-mate directory

--- budget-mate/src/main/java/com/budget/budgetmate/repositories/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.budget.budgetmate.repositories;
-import com.budget.budgetmate.models.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-    List<Account> findByUserId(Long userId);
-}
